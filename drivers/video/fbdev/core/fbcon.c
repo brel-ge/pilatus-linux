e534ca4893fb7e7ffa4ae93ce89e5c48d0484b17
Ks <uwe.kaiser@brel.ch>
2024-06-17 13:10:37 +0200

    keep logo visible during quiet boot

--- drivers/video/fbdev/core/fbcon.c
+++ drivers/video/fbdev/core/fbcon.c
@@ -1012,8 +1012,8 @@ static void fbcon_init(struct vc_data *vc, bool init)
 
 	info = fbcon_info_from_console(vc->vc_num);
 
-	if (logo_shown < 0 && console_loglevel <= CONSOLE_LOGLEVEL_QUIET)
-		logo_shown = FBCON_LOGO_DONTSHOW;
+	// if (logo_shown < 0 && console_loglevel <= CONSOLE_LOGLEVEL_QUIET)
+	// 	logo_shown = FBCON_LOGO_DONTSHOW;
 
 	if (vc != svc || logo_shown == FBCON_LOGO_DONTSHOW ||
 	    (info->fix.type == FB_TYPE_TEXT))
